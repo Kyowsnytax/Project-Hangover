13b5c45aeb0b3b87ee597be2765439793b3f8484
JiverDev <lorenceolivera1039@gmail.com>
2025-11-17 22:24:15 +0800

    icontest2

--- about.php
+++ about.php
@@ -12,7 +12,7 @@ session_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Hang Over</title>
-    <link rel="icon" type="image/png" href="Images/GIF LOGO.gif">
+    <link rel="icon" type="image/png" href="Images/gif logo.gif">
     
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
